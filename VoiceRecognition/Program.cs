8eed40816587331080240ef8e1c9cb435a27269d
Thanh <thanh.hoangxuan@alfalaval.com>
2023-04-03 12:32:13 +0200

    poc

--- VoiceRecognition/Program.cs
+++ VoiceRecognition/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Google.Cloud.Speech.V1;
 using Google.Apis.Auth.OAuth2;
 using System;
+using System.Text;
 
 namespace SpeechToTextApiDemo
 {
@@ -10,13 +11,23 @@ namespace SpeechToTextApiDemo
         {
             System.Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "C:\\Users\\hxt\\source\\repos\\Marriot Solution\\VoiceRecognition\\VoiceRecognition\\key.json");
             var speech = SpeechClient.Create();
+            var DiarizationConfig1 = new SpeakerDiarizationConfig
+            {
+                EnableSpeakerDiarization = true,
+                MinSpeakerCount = 1,
+                MaxSpeakerCount = 6,
+            };
             var config = new RecognitionConfig
             {
-                Encoding = RecognitionConfig.Types.AudioEncoding.Flac,
+                Encoding = RecognitionConfig.Types.AudioEncoding.EncodingUnspecified,
                 SampleRateHertz = 16000,
-                LanguageCode = LanguageCodes.English.UnitedStates
+                EnableSpokenPunctuation = true,
+                EnableAutomaticPunctuation = true,
+                DiarizationConfig = DiarizationConfig1,
+                EnableWordConfidence = true,
+                LanguageCode = LanguageCodes.English.UnitedKingdom
             };
-            var audio = RecognitionAudio.FromStorageUri("gs://cloud-samples-tests/speech/brooklyn.flac");
+            var audio = RecognitionAudio.FromFile("C:\\Users\\hxt\\source\\repos\\Marriot Solution\\VoiceRecognition\\VoiceRecognition\\short-test.mp3");
 
             var response = speech.Recognize(config, audio);
 
@@ -24,8 +35,16 @@ namespace SpeechToTextApiDemo
             {
                 foreach (var alternative in result.Alternatives)
                 {
-                    Console.WriteLine(alternative.Transcript.Words.Count);
+                    foreach (var work in alternative.Words)
+                    {
+                        Console.OutputEncoding = Encoding.UTF8;
+                        Console.WriteLine("Work: " + work.Word + "   |   "+ "Confident: " + work.Confidence);
+                    }
+
+                    Console.WriteLine(alternative.Transcript);
+                    Console.WriteLine("End a sentence \n");
                 }
+                    
             }
         }
     }

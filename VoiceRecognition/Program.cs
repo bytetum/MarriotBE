ff73ebb7d317c9c20423723dcbcbb3e588989b3b
Thanh Hoang Xuan <thanh.hoangxuan@alfalaval.com>
2023-04-05 18:05:52 +0200

    feat: search tu poc

--- VoiceRecognition/Program.cs
+++ VoiceRecognition/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Google.Cloud.Speech.V1;
+using Newtonsoft.Json.Linq;
+using System.Linq;
 
 public class SpeechToTextTranscriber
 {
@@ -33,6 +36,7 @@ public class SpeechToTextTranscriber
             LanguageCode = LanguageCodes.Vietnamese.Vietnam,
             Model = "latest_long",
             DiarizationConfig = diariConfig,
+            UseEnhanced = true,
         };
 
         var audio = RecognitionAudio.FromFile(inputAudioFilePath);
@@ -46,7 +50,7 @@ public class SpeechToTextTranscriber
             return;
         }
 
-        var lastResult = completedResponse.Result.Results[completedResponse.Result.Results.Count - 1];
+        /*var lastResult = completedResponse.Result.Results[completedResponse.Result.Results.Count - 1];
         var wordsInfo = lastResult.Alternatives[0].Words;
 
         int tag = 1;
@@ -64,31 +68,70 @@ public class SpeechToTextTranscriber
                 transcript += $"speaker {tag}: {speaker}\n";
                 tag = wordInfo.SpeakerTag;
                 speaker = wordInfo.Word;
+
+                
             }
         }
 
         transcript += $"speaker {tag}: {speaker}";
         Console.OutputEncoding = Encoding.UTF8;
-        Console.WriteLine(transcript);
+        Console.WriteLine(transcript);*/
 
-        /*var speakersTranscripts = new Dictionary<int, List<string>>();
-        
-        foreach (var result in completedResponse.Result.Results)
-        {
-            foreach (var wordInfo in result.Alternatives[0].Words)
+        var speakersTranscripts = new Dictionary<int, List<string>>();
+        Console.OutputEncoding = Encoding.UTF8;
+        foreach (var wordInfo in completedResponse.Result.Results[completedResponse.Result.Results.Count - 1].Alternatives[0].Words)
             {
                 if (!speakersTranscripts.ContainsKey(wordInfo.SpeakerTag))
                 {
                     speakersTranscripts[wordInfo.SpeakerTag] = new List<string>();
                 }
-
+               // Console.WriteLine(wordInfo.Word + "  " + wordInfo.SpeakerTag);
                 speakersTranscripts[wordInfo.SpeakerTag].Add(wordInfo.Word);
             }
-        }
+
+        string[] myCombination = { "vật", "chất"};
+        Console.WriteLine($"Tổ hợp từ cần tìm  {string.Join(" ", myCombination)}");
+        Console.WriteLine("\n");
+
 
         foreach (var speaker in speakersTranscripts.Keys)
         {
             Console.WriteLine($"Speaker {speaker}: {string.Join(" ", speakersTranscripts[speaker])}");
-        } */
+            if (ContainsExactCombination(speakersTranscripts[speaker].ToArray(), myCombination.ToArray()))
+            {
+                Console.WriteLine("Found the exact combination of items");
+
+                Console.WriteLine("\n");
+                Console.WriteLine("\n");
+            }
+            else
+            {
+                Console.WriteLine("Did not find the exact combination of items");
+
+                Console.WriteLine("\n");
+
+                Console.WriteLine("\n");
+            }
+        } 
+    }
+    public static bool ContainsExactCombination(string[] array, string[] combination)
+    {
+        for (int i = 0; i <= array.Length - combination.Length; i++)
+        {
+            bool match = true;
+            for (int j = 0; j < combination.Length; j++)
+            {
+                if (array[i + j] != combination[j])
+                {
+                    match = false;
+                    break;
+                }
+            }
+            if (match)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
